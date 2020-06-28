7ec28a39772ec6148fc601b71d29c4f474b84447
Viking & Robot <vikingandrobot@gmail.com>
2020-06-28 21:02:34 +0200

    Starter kit project n°2.

---
+++ starter_kit/starterkit_project_02/starterkit_project_02.ino
@@ -0,0 +1,31 @@
+int switchState = 0;
+
+void setup() {
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(2, INPUT);
+}
+
+void loop() {
+  switchState = digitalRead(2);
+
+  if (switchState == HIGH) {
+    digitalWrite(3, HIGH); // green
+    digitalWrite(4, LOW); // red
+    digitalWrite(5, LOW); // red
+  } else {
+    digitalWrite(3, LOW); // green
+
+    // Blink the mofos
+    digitalWrite(4, LOW); // red
+    digitalWrite(5, HIGH); // red
+
+    delay(250);
+
+    digitalWrite(4, HIGH); // red
+    digitalWrite(5, LOW); // red
+
+    delay(250);
+  }
+}
